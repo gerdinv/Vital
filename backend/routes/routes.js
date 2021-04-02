332fafc7ba237f256e8cdee0a3ca0055197339e1
gerdinv <gerdinventura2@gmail.comm>
2021-04-02 02:07:33 -0400

    Requests to signup add users to the database

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,21 +1,22 @@
 // File is responsible for routing the requests that come to the server
 
-const { request, response } = require('express')
+// const { request, response } = require('express')
 const express = require('express')
 const router = express.Router()
-const UserSchema = require('./models/signUpModel.js')
+const UserSchema = require('../models/userModel')
 
 router.post('/signup', (req, res) => {
     const newUser = new UserSchema({
-        fullName: request.body.fullName,
-        username: request.body.username,
-        email: request.body.email,
-        password: request.body.password
+        fullname: req.body.fullname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
     })
     newUser.save().then(data => {
-        response.json(data)
+        res.json(data)
     }).catch(err => {
-        response.json(err)
+        res.json(err)
+        console.log("HEY")
     })
 })
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const app = express()
+const routesUrls = require('./routes/routes')
+const cors = require('cors')
 
 dotenv.config()
-mongoose.connect(process.env.DATABASE_ACCESS, {useNewUrlParser: true}, () => {
+
+mongoose.connect(process.env.DATABASE_ACCESS, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Database connected!")
 })
+
+app.use(express.json())
+app.use(cors())
+app.use('/app', routesUrls)
 
 app.listen(4000, () => {
     console.log("Server is running!")
 })
+
+
+
