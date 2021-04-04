cac67b892569918882b457012de2b9bfd8fdcc84
gerdinv <gerdinventura2@gmail.comm>
2021-04-04 06:42:39 -0400

    updated UI, added card components

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Redirect } from 'react-router';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Post from '../Posts/postBox'
+import './home.css'
 
-function Home (props) {
-    
+function Home(props) {
+    let menu;
+
+    if(props.name !== ''){
+        menu = (
+            <div>
+                <Post className="box" feature="primary" />
+                <Post className="box" feature="success" />
+                <Post className="box" feature="warning" />
+                <Post feature="danger" />
+            </div>
+        )
+    }
     return (
-        <h1> {props.name ? "Welcome " + props.name : "You must log in!"}</h1>
+        <Container >
+            <Row>
+                <h1> {props.name ? "Welcome " + props.name : "You must log in!"}</h1>
+            </Row>
+            
+           
+            {menu}
+            
+            
+
+            
+
+            
+        </Container>
+
     );
 }
 
